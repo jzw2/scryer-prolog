4862074bdf8899d99694c2d0a8a80f640467c0df
John Wang <jzw2@illinois.edu>
2023-06-21 08:05:11 +0200

    Put it in teh wrong place

--- flake.nix
+++ flake.nix
@@ -11,22 +11,35 @@
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
       in
-      {
-        defaultPackage = naersk-lib.buildPackage {
-          src = ./.;
-      nativeBuildInputs = with pkgs; [ pkg-config ];
-        buildInputs = with pkgs; [ openssl gmp libmpc mpfr
-                                   libffi
-                          gnum4];
-        };
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy
+        {
+          defaultPackage = naersk-lib.buildPackage {
+            src = ./.;
 
-                        ];
+            singleStep = true;
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+            buildInputs = with pkgs; [ openssl gmp libmpc mpfr
+                                       libffi
+                                       gnum4];
+            preConfigure = ''
+    mkdir /tmp/deps
+    rsync -aL $crate_sources/ /tmp/deps
+    chmod 777 -R /tmp/deps
+    crate_sources=/tmp/deps
+  '';
+
+            preBuild = ''
+    echo '[source]' > $CARGO_HOME/config
+    echo '"crates-io" = { "replace-with" = "nix-sources" }' >> $CARGO_HOME/config
+    echo '"nix-sources" = { directory = "/tmp/deps" }' >> $CARGO_HOME/config
+  '';
+          };
+          devShell = with pkgs; mkShell {
+            buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy
+
+                          ];
 
 
-
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
-      });
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+        });
 }
